3fd8407b75fadd905769cf859f7a73d73c785bfe
kyanezca <37675454+kyanezca@users.noreply.github.com>
2018-07-20 15:13:08 -0700

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 
 $( document ).ready(function() {
 $('.single-item').slick({
-  autoplay:true,
-  autoplaySpeed:2000,
+  // autoplay:true,
+  autoplaySpeed:4000,
   arrows:true,
+  speed: 2000,
   prevArrow:'<button type="button" class="slick-prev"></button>',
   nextArrow:'<button type="button" class="slick-next"></button>',
   });
--- page.php
+++ page.php
@@ -3,34 +3,20 @@
 		<div class="column small-12 medium-9 main-feed">
 				
     				<!-- slide goes here --><?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<header class="header">
-<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-</header>
-<section class="entry-content">
-<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-<?php the_content(); ?>
-<div class="entry-links"><?php wp_link_pages(); ?></div>
-</section>
-</article>
+
 <?php if ( ! post_password_required() ) comments_template( '', true ); ?>
 <?php endwhile; endif; ?>
 			<hr>
-					<!-- individual feed post goes here -->
-				
-<!-- // Define our WP Query Parameters -->
-<?php $the_query = new WP_Query( 'posts_per_page=5' ); ?>
- 
-<!-- // Start our WP Query -->
-
+<ul class="single-item">
+<li>
  <?php
 	                $args = array(
 	                    'post_type' => 'post',
 	                    'post_status' => 'publish',
-	                    // 'category_name' => 'news',
+	                    // 'category_name' => 'carousel',
 	                    'orderby' => 'date',
-	                    'order' => 'ASC',
-	                    'posts_per_page' => 10
+	                    'order' => 'DEC',
+	                    'posts_per_page' => 5
 	                );
 	                $arr_posts = new WP_Query( $args );
 
@@ -38,7 +24,40 @@
 	                    while ( $arr_posts->have_posts() ) :
 	                        $arr_posts->the_post();
 	                        ?>
-	                        <div class="news-feed column small-12 medium-12">
+	                        <li><?php the_post_thumbnail(); ?><span><a href="<?php the_permalink() ?>"><?php the_title(); ?></a><?php the_excerpt(__('(more…)')); ?></span></li>
+	           
+
+
+	                       
+	                        <?php
+	                    endwhile;
+	                endif;
+                ?></li>
+</ul>
+					<!-- individual feed post goes here -->
+				
+<!-- // Define our WP Query Parameters -->
+<?php $the_query = new WP_Query( 'posts_per_page=5' ); ?>
+ 
+<!-- // Start our WP Query -->
+
+ <ul id="news-feed">
+ <?php
+	                $args = array(
+	                    'post_type' => 'post',
+	                    'post_status' => 'publish',
+	                    // 'category_name' => 'news',
+	                    'orderby' => 'date',
+	                    'order' => 'DEC',
+	                    'posts_per_page' => 15
+	                );
+	                $arr_posts = new WP_Query( $args );
+
+	                if ( $arr_posts->have_posts() ) :
+	                    while ( $arr_posts->have_posts() ) :
+	                        $arr_posts->the_post();
+	                        ?>
+	                        <li class="news-feed column small-12 medium-12">
 				<div class="story-feed">
 					<div class="image-feed column small-12 medium-3">
 							<!-- post image goes here -->
@@ -57,7 +76,8 @@
 						</div>
 					</div>	
 				</div>
-			</div>
+			</li>
+
 
 
 	                       
@@ -66,7 +86,7 @@
 	                endif;
                 ?>
 
-					
+				</ul>	
 		</div>
 			<?php get_sidebar(); ?>
 	</div>
